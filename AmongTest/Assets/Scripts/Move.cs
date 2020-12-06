7246aa172fca09e038b9818898775cb9a174371c
amir-2708 <amir.2708@yahoo.de>
2020-12-06 19:53:44 +0100

    AmongTest
    Altes Multiplayer

---
+++ AmongTest/Assets/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : Photon.Pun.MonoBehaviourPun
+{
+    public float speed = 15;
+    Vector2 velocity;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        velocity = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (photonView)
+        rb.MovePosition(rb.position + velocity * speed * Time.fixedDeltaTime);
+    }
+
+}
---
+++ AmongTest/Assets/Scripts/Network.cs
@@ -0,0 +1,38 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Network : MonoBehaviourPunCallbacks
+{
+    public Text statusText;
+    public CameraFollow playerCamera;
+
+    
+    private void Start()
+    {
+        statusText.text = "Connecting";
+        PhotonNetwork.NickName = "Player" + Random.Range(0, 5000);
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        
+        statusText.text = "Joining room";
+        PhotonNetwork.JoinOrCreateRoom("Room2456", new RoomOptions() { MaxPlayers = 4 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        statusText.text = "Connected ";
+        playerCamera.target = PhotonNetwork.Instantiate("Player", 
+            new Vector3(
+                Random.Range(-4, 4), 
+                Random.Range(-4, 4), 
+                0), Quaternion.identity).transform;
+    }
+}
