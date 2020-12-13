53876eda26b981f399f14fb9493e51b744ebb88c
amir-2708 <amir.2708@yahoo.de>
2020-12-13 15:36:48 +0100

    Lobby Update 0.2
    update

---
+++ Lobby_Beta 0.2/AmongTest/Assets/Scripts/Network.cs
@@ -0,0 +1,37 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Network : MonoBehaviourPunCallbacks
+{
+    public Text statusText;
+    public CameraFollow playerCamera;
+    public GameObject Lobby_Room_Name;
+
+    public void CreateLobby()
+    {
+        statusText.text = "Connecting";
+        PhotonNetwork.NickName = "Player" + Random.Range(0, 100);
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        statusText.text = "Joining room";
+        PhotonNetwork.JoinOrCreateRoom(Lobby_Room_Name.GetComponent<Text>().text, new RoomOptions() { MaxPlayers = 10 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        statusText.text = "Connected to Lobby: " + Lobby_Room_Name.GetComponent<Text>().text;
+        playerCamera.target = PhotonNetwork.Instantiate("Player", 
+            new Vector3(
+                Random.Range(-4, 4), 
+                Random.Range(-4, 4), 
+                0), Quaternion.identity).transform;
+    }
+}
