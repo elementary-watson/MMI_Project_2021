7503076ac2d5e569b74d5b76efebc0a2dcdfa0ab
elementary-watson <rafatim@hotmail.de>
2020-12-26 13:11:11 +0100

    keine ahnung

--- Lobby_Beta 0.4/AmongTest/Assets/Scripts/Network.cs
+++ Lobby_Beta 0.4/AmongTest/Assets/Scripts/Network.cs
@@ -2,15 +2,19 @@
 using Photon.Pun;
 using Photon.Realtime;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Network : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private TMP_Text[] playerNameTexts = new TMP_Text[4];
+    [SerializeField] private TMP_Text[] playerReadyTexts = new TMP_Text[4];
+    [SerializeField] private Image[] playerImageContainer = new Image[4];
     public Text statusText;
     public CameraFollow playerCamera;
-    public GameObject Lobby_Room_Name;
+    public string Lobby_Room_Name;
 
     PhotonView photonView;
     public Text statusPlayerNumber;
@@ -20,11 +24,11 @@ public class Network : MonoBehaviourPunCallbacks
 
     private void SpawnPlayer(string PlayerColor)
     {
-        playerCamera.target = PhotonNetwork.Instantiate(PlayerColor,
+       /*playerCamera.target = PhotonNetwork.Instantiate(PlayerColor,
                 new Vector3(
                     Random.Range(-4, 4),
                     Random.Range(-4, 4),
-                    0), Quaternion.identity).transform;
+                    0), Quaternion.identity).transform;*/
     }
 
     private string RandomColor()
@@ -62,46 +66,50 @@ public class Network : MonoBehaviourPunCallbacks
         return PlayerColorName[randomNumberforColor];
     }
 
+    public void Awake()
+    {
+        Lobby_Room_Name = "LobbyA";
+        playerImageContainer[0].enabled = false;
+        playerImageContainer[1].enabled = false;
+        playerImageContainer[2].enabled = false;
+        playerImageContainer[3].enabled = false;
+    }
+
     [PunRPC]
     public void RefreshPlayerNumberLogical()
     {
         statusPlayerNumber.text = "(" + PhotonNetwork.CurrentRoom.PlayerCount + "/10)";
     }
 
-    public void CreateLobby()
+    [PunRPC]
+    public void SyncPlayersTextfiled()
     {
-        statusText.text = "Connecting";
-        PhotonNetwork.NickName = "Player" + Random.Range(0, 100);
-
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        statusText.text = "Joining room";
-        PhotonNetwork.JoinOrCreateRoom(Lobby_Room_Name.GetComponent<Text>().text, new RoomOptions() { MaxPlayers = 10 }, null);
-    }
-
-    public override void OnJoinedRoom()
-    {
-        statusText.text = "Connected to Lobby: " + Lobby_Room_Name.GetComponent<Text>().text;
-
-        photonView = gameObject.GetComponent<PhotonView>();
-        photonView.RPC("RefreshPlayerNumberLogical", RpcTarget.All);
-
         switch (PhotonNetwork.CurrentRoom.PlayerCount)
         {
             case 1:
                 SpawnPlayer(RandomColor());
+                playerImageContainer[0].enabled = true;
+                playerNameTexts[0].SetText(PhotonNetwork.NickName);                
+                playerReadyTexts[0].SetText("LET IT RIP");
+
                 break;
             case 2:
                 SpawnPlayer(RandomColor());
+                playerImageContainer[1].enabled = true;
+                playerNameTexts[1].SetText(PhotonNetwork.NickName);
+                playerReadyTexts[1].SetText("LET IT RIP");
                 break;
             case 3:
                 SpawnPlayer(RandomColor());
+                playerImageContainer[2].enabled = true;
+                playerNameTexts[2].SetText(PhotonNetwork.NickName);
+                playerReadyTexts[2].SetText("LET IT RIP");
                 break;
             case 4:
                 SpawnPlayer(RandomColor());
+                playerImageContainer[3].enabled = true;
+                playerNameTexts[3].SetText(PhotonNetwork.NickName);
+                playerReadyTexts[3].SetText("LET IT RIP");
                 break;
             case 5:
                 SpawnPlayer(RandomColor());
@@ -125,7 +133,38 @@ public class Network : MonoBehaviourPunCallbacks
                 print("Das darf gar nicht passieren");
                 break;
         }
-       
+
+    }
+
+    public void CreateLobby()
+    {
+        statusText.text = "Connecting";
+        PhotonNetwork.NickName = "Player" + Random.Range(0, 1000);
+        
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        statusText.text = "Joining room";
+        PhotonNetwork.JoinOrCreateRoom(Lobby_Room_Name, new RoomOptions() { MaxPlayers = 10 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //Dictionary<int, string> openWith = PhotonNetwork.CurrentRoom.;
+        //Player playerList[] = new playerList[PhotonNetwork.PlayerList];
+
+        foreach (Player player in PhotonNetwork.PlayerList) 
+        {
+            print("ID: "+ player.ActorNumber + "\n" + "Nickname: " + player.NickName);
+        }
+
+        Debug.Log("DEBUG: Name of Player: " + PhotonNetwork.NickName);
+        statusText.text = "Connected to Lobby: " + Lobby_Room_Name;
+        photonView = gameObject.GetComponent<PhotonView>();
+        photonView.RPC("RefreshPlayerNumberLogical", RpcTarget.All);
+        photonView.RPC("SyncPlayersTextfiled", RpcTarget.AllBuffered);
         //statusPlayerNumber.text = "("+ PhotonNetwork.CurrentRoom.PlayerCount + "/10)";
     }
 }
