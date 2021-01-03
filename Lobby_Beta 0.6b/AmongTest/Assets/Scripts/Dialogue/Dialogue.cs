68822ddec072646a730f77534950a1f1fe544b4f
amir-2708 <amir.2708@yahoo.de>
2021-01-03 23:07:22 +0100

    MapCounterB

---
+++ Lobby_Beta 0.6b/AmongTest/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue
+{
+        //wird als Objekt benutzt für DialogueManager deshalb kein MonoBehaviour
+        public string name;
+        public string[] sentences;
+
+}
---
+++ Lobby_Beta 0.6b/AmongTest/Assets/Scripts/RPC.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+
+public class RPC : MonoBehaviour
+{
+    PhotonView photonView;
+    public Text updateText;
+    int counter = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = gameObject.GetComponent<PhotonView>();
+        updateText.text = "" + counter;
+        print(counter);
+    }
+
+
+    
+
+    public void rufe_RPC_auf()
+    {
+        photonView.RPC("refresh", RpcTarget.All);
+    }
+
+
+    [PunRPC]
+    public void refresh()
+    {
+        counter += 1;
+        updateText.text = "" + counter;
+        print(counter);
+    }
+}
