9263372dd7ae9db8240eb7a38e36477a3e79e696
elementary-watson <rafatim@hotmail.de>
2021-01-03 21:57:39 +0100

    Chat Ftr Commit

---
+++ Lobby_Beta 0.6/AmongTest/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public void setTutorial()
+    {
+        List<string> tutorialList = new List<string>();
+        tutorialList.Add("Hi");
+        tutorialList.Add("This is a turorial.");
+        tutorialList.Add("Thank you, have fun!");
+        string[] str = tutorialList.ToArray();
+
+        dialogue = new Dialogue { name = "Teacher", sentences = str };
+
+        TriggerDialogue(dialogue);
+    }
+
+    public void TriggerDialogue(Dialogue dialogue)
+    {
+
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
---
+++ Lobby_Beta 0.6/AmongTest/Assets/Scripts/Test.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    //List<string> PlayerNamen = new List<string>();
+
+   //string Spielername = "Amir";
+ 
+
+
+    /*string StringTest(string a)
+    {
+        print(a);
+        return a;
+    }*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* PlayerNamen.Add("PlayerBlack");
+         PlayerNamen.Add("PlayerBlue");
+         PlayerNamen.Add("PlayerBrown");
+         PlayerNamen.Add("PlayerCyan");
+         PlayerNamen.Add("PlayerGreen");
+         PlayerNamen.Add("PlayerOrange");
+         PlayerNamen.Add("PlayerPurple");
+         PlayerNamen.Add("PlayerRed");
+         PlayerNamen.Add("PlayerWhite");
+         PlayerNamen.Add("PlayerYellow");
+
+
+         switch (Spielername)
+         {
+             case 1:
+                 print("Ich heiße Amir");
+                 break;
+             case 2:
+                 print("Ich heiße 123");
+                 break;
+             case 3:
+                 print("Ich heiße 234");
+                 break;
+             case 4:
+                 print("Ich heiße 345");
+                 break;
+             case "456":
+                 print("Ich heiße 456");
+                 break;
+             default:
+                 print("Ich hab mein Namen vergessen");
+                 break;
+         }*/
+
+    }
+
+
+
+}
