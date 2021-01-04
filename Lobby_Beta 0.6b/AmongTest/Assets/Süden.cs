19bdf22ef2048813db20f8bc1c2b6dd387c833e4
amir-2708 <amir.2708@yahoo.de>
2021-01-04 18:00:40 +0100

    SüdenTest3

---
+++ Lobby_Beta 0.6b/AmongTest/Assets/Süden.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Süden : MonoBehaviour
+{
+    [SerializeField] private Button clickButton;
+    public Text changeText;
+        
+
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        changeText.text = "Hello you are in Süden";    
+    }
+}
