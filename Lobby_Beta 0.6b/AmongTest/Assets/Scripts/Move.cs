42ef01580d1ebfa27113d11be05748636cb3f206
amir-2708 <amir.2708@yahoo.de>
2021-01-07 10:14:00 +0100

    FirstMapDesign

--- Lobby_Beta 0.6b/AmongTest/Assets/Scripts/Move.cs
+++ Lobby_Beta 0.6b/AmongTest/Assets/Scripts/Move.cs
@@ -4,33 +4,56 @@ using UnityEngine;
 
 public class Move : Photon.Pun.MonoBehaviourPun
 {
-    public float speed = 5;
-    Vector2 velocity;
-    Rigidbody2D rb;
+    private float speed = 8;
+    //Vector2 velocity;
+    //private Rigidbody2D rb;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
 
-    private void Awake()
+    /*private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         velocity = Vector2.zero;
-    }
+    }*/
 
-    private void Update()
+    private void Start()
     {
-
-        if (photonView.IsMine)
-        {
-            velocity.x = Input.GetAxisRaw("Horizontal");
-            velocity.y = Input.GetAxisRaw("Vertical");
-        }
-            
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
         if (photonView.IsMine)
         {
-            rb.MovePosition(rb.position + velocity * speed * Time.fixedDeltaTime);
+            change = Vector3.zero;
+            change.x = Input.GetAxisRaw("Horizontal");
+            change.y = Input.GetAxisRaw("Vertical");
+
+            if (change != Vector3.zero)
+            {
+                MoveCharacter();
+            }
+
+            //velocity.x = Input.GetAxisRaw("Horizontal");
+            //velocity.y = Input.GetAxisRaw("Vertical");
         }
     }
+
+    private void MoveCharacter()
+    {
+        if (photonView.IsMine)
+        {
+            myRigidbody.MovePosition(transform.position + change * speed * Time.fixedDeltaTime);
+        }
+    }
+
+
+    /*private void FixedUpdate()
+    {
+        if (photonView.IsMine)
+        {
+            rb.MovePosition(rb.position + velocity * speed * Time.fixedDeltaTime);
+        }
+    }*/
 
 }
