372fd7daef3096e73cc3f226779238f040dacf6e
elementary-watson <rafatim@hotmail.de>
2021-01-26 23:04:47 +0100

    Zwischenstand

--- Lobby_Beta 0.7/AmongTest/Assets/Scripts/User Interface/MultiplayerProgressBar.cs
+++ Lobby_Beta 0.7/AmongTest/Assets/Scripts/User Interface/MultiplayerProgressBar.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MultiplayerProgressBar : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int maximum;
+    public int current;
     void Start()
     {
         
@@ -14,5 +17,11 @@ public class MultiplayerProgressBar : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GetCurrentFill()
+    {
+        float fillAmount = (float)current / (float)maximum;
+        //Mask.fillAmount = fillAmount;
     }
 }
