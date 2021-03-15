037fb248d6f7b367f79c826b9b843603b37fbdcc
elementary-watson <rafatim@hotmail.de>
2021-03-15 01:08:03 +0100

    task fill update

--- Lobby_Beta 0.9/AmongTest/Assets/single_fillgauge_task.cs
+++ Lobby_Beta 0.9/AmongTest/Assets/single_fillgauge_task.cs
@@ -5,16 +5,20 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class single_fillgauge_task : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class Single_Fillgauge_Task : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField] Main_Fillgauge_Task mft_object;
     public Button btn_fill;
     public Image img_fillGauge;
+    [SerializeField] Image[] img_randomVisuals = new Image[0];
     [SerializeField] float current;
     [SerializeField] float maximum;
-    bool pointerUp;
+    bool isInteractable;
     bool pointerDown;
     public UnityEvent onHoldClick;
-
+    float start = 0.15f;
+    float end = 0.90f;
+    int randomValue;
     public void OnPointerDown(PointerEventData eventData)
     {
         pointerDown = true;
@@ -22,34 +26,71 @@ public class single_fillgauge_task : MonoBehaviour, IPointerDownHandler, IPointe
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Reset();
-        
+        print("pointup");
+        print(randomValue);
+        print("Current: " + current);
+        if (current > 0.30f && current < 0.40f && randomValue == 0)
+        {
+            isInteractable = false; disableBtn(); mft_object.setCurrent();
+        }
+        else if (current > 0.55f && current < 0.65f && randomValue == 1)
+        {
+            isInteractable = false; disableBtn();
+        }
+        else if (current > 0.80f && current < 0.90f && randomValue == 2)
+        {
+            isInteractable = false; disableBtn();
+        }
+        Reset();        
     }
     public void Reset() 
     {
-        pointerDown = false;
-        current = 0.13f;
-        img_fillGauge.fillAmount = current / maximum;
+        if (isInteractable)
+        {
+            pointerDown = false;
+            current = start;
+            img_fillGauge.fillAmount = current / maximum;
+        }
     }
-    // Start is called before the first frame update
     void Start()
     {
-        img_fillGauge.fillAmount = 0.13f;
+        randomValue = Random.Range(0,2);
+        if (randomValue == 0) img_randomVisuals[0].enabled = true;
+        if (randomValue == 1) img_randomVisuals[1].enabled = true;
+        if (randomValue == 2) img_randomVisuals[2].enabled = true;
+             isInteractable = true;
+        img_fillGauge.fillAmount = start;
+        current = start;
+        maximum = end;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (pointerDown)
+        if (isInteractable) 
         {
-            current += Time.deltaTime;
-            if(current >= maximum)
+            if (pointerDown)
             {
-                if (onHoldClick != null)
-                    onHoldClick.Invoke();
-                Reset();
+                current += (Time.deltaTime)/3;
+                //print("Current1: " + current);
+                if (current >= maximum)
+                {
+                    print("Current2: " +current);
+                    if (onHoldClick != null)
+                    {
+                        isInteractable = false;
+                        onHoldClick.Invoke(); 
+                    }
+                }
+                img_fillGauge.fillAmount = current / maximum;
             }
-            img_fillGauge.fillAmount = current / maximum;
+        }        
+    }
+    public void disableBtn()
+    {
+        if (!isInteractable)
+        {
+            btn_fill.enabled = false;
+            isInteractable = false;
+            print("btncall");
         }
     }
     public void btn_startFillgauge()
@@ -69,5 +110,6 @@ public class single_fillgauge_task : MonoBehaviour, IPointerDownHandler, IPointe
         }
 
     }
+
 
 }
