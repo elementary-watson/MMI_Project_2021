fb9808e75ef22ea9f9314a3cb8c5e37355d86f31
elementary-watson <rafatim@hotmail.de>
2021-03-23 15:44:30 +0100

    up

--- Lobby_Beta 0.9/AmongTest/Assets/Multiplayer_Reference.cs
+++ Lobby_Beta 0.9/AmongTest/Assets/Multiplayer_Reference.cs
@@ -32,8 +32,8 @@ public class Multiplayer_Reference : MonoBehaviour
     List<string> RandomColorList;
 
     List<string> AllTasksList = new List<string> { // XOF AMIR
-        //"Tag_WaterDispenser", "Tag_Game", "Tag_NumberRadio",
-        "Tag_Fillgauge","Tag_Fillgauge","Tag_Fillgauge","Tag_NumberBox", "Tag_ElectricBox", "Tag_ClickCabinet",
+        "Tag_WaterDispenser", "Tag_Game", "Tag_NumberRadio",
+        "Tag_NumberBox", "Tag_ElectricBox", "Tag_ClickCabinet",
         "Tag_Fillgauge", "Tag_EnergyNumber", "Tag_LeverEnergy",
         "Tag_ClickMediKit", "Tag_Sink", "Tag_Tablet",
         "Tag_ComputerLabor", "Tag_LaborSingleTube", "Tag_ClickLabor",
--- Lobby_Beta 0.9/AmongTest/Assets/Scripts/Network.cs
+++ Lobby_Beta 0.9/AmongTest/Assets/Scripts/Network.cs
@@ -104,8 +104,8 @@ public class Network : MonoBehaviourPunCallbacks
             playerReadyTexts[i].text = "";
         }
         RandomColor();
-        myRoomOptions = new RoomOptions() { MaxPlayers = 1, IsVisible = true, IsOpen = true /*,PlayerTtl = 10000, EmptyRoomTtl=60000*/ };
-        maxPlayersOfRoom = 1;
+        myRoomOptions = new RoomOptions() { MaxPlayers = 4, IsVisible = true, IsOpen = true /*,PlayerTtl = 10000, EmptyRoomTtl=60000*/ };
+        maxPlayersOfRoom = 4;
         //canJoin = true;
     }
 
